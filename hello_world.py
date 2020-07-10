fe71e6cee873043693deb48bfab56f5ad44fcff5
Adolfo <psyal3@nottingham.ac.uk>
2020-07-10 18:28:22 +0200

    Chapter 4

--- hello_world.py
+++ hello_world.py
@@ -1,38 +1,106 @@
-#Python 3.7
+# Python 3.7
 intro_msg = ["hello world", "hi universe", "hey python"]
 
-#sort list alphabetically
-#intro_msg.sort()
+print("--------------------------------------------------")
+print("chapters 1, 2, 3".title().center(50))
+print("--------------------------------------------------")
 
-#sort list alphabetically (reversed)
-#intro_msg.sort(reverse=True)
+# sort list alphabetically
+# intro_msg.sort()
 
-#sort a list temporarily
-#print(sorted(intro_msg))
+# sort list alphabetically (reversed)
+# intro_msg.sort(reverse=True)
 
-#print list in reverse order
-#intro_msg.reverse()
+# sort a list temporarily
+# print(sorted(intro_msg))
 
-#appending elements to the end of a list
-#intro_msg.append("mic check")
+# print list in reverse order
+# intro_msg.reverse()
 
-#inserting elements into a list
-#intro_msg.insert(0, "knock knock")
+# appending elements to the end of a list
+# intro_msg.append("mic check")
 
-#deleting an element from a list
-#del intro_msg[0]
+# inserting elements into a list
+# intro_msg.insert(0, "knock knock")
 
-#popping an element from a list
-#popped_intro_msg= intro_msg.pop()
+# deleting an element from a list
+# del intro_msg[0]
 
-#pop from any position in a list
-#second_msg = intro_msg.pop(1)
+# popping an element from a list
+# popped_intro_msg= intro_msg.pop()
 
-#removing an item by value
-#intro_msg.remove("hi universe")
+# pop from any position in a list
+# second_msg = intro_msg.pop(1)
 
-#finding length of a list
-#print(len(intro_msg))
+# removing an item by value
+# intro_msg.remove("hi universe")
+
+# finding length of a list
+# print(len(intro_msg))
 
 print(intro_msg[0].title() + "! I'm new to Python...\n")
-print(intro_msg)
+# print(intro_msg)
+
+print("\n" + "--------------------------------------------------")
+print("chapter 4: working with lists".title().center(50))
+print("--------------------------------------------------")
+
+# iterate through list
+for message in intro_msg:
+    print(message.title() + "! ", end = '')
+
+print("\n")
+
+# create numerical lists with range()
+for value in range(1, 4):
+    print("I've completed chapter ".title() + str(value) + " ☑\n")
+
+# If you want to make a list of numbers, you can convert the results of range()
+# directly into a list using the list() function. When you wrap list() around
+# a call to the range() function, the output will be a list of numbers.
+numbers = list(range(1, 6))
+print("List of numbers from 1-5: " + str(numbers))
+
+# use the range() function to tell Python to skip numbers in a given range.
+even_numbers = list(range(2,11,2))
+print("List of even numbers from 1-10: " + str(even_numbers))
+
+# make a list of the first 10 square numbers
+square_numbers = []
+for value in range(1,11):
+    square = value**2
+    square_numbers.append(square)
+
+print("List of square numbers from 1-10: " + str(square_numbers))
+
+# finding the minimum, maximum and sum of a list
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+print("\nSum of list: " + str(sum(digits)))
+print("Maximum of list: " + str(max(digits)))
+print("Minimum of list: " + str(min(digits)))
+
+# list comprehension
+square_numbers = [value**2 for value in range(1,11)]
+print("\nList comprehension of square numbers from 1-10: " + str(square_numbers))
+
+# slicing a list
+print("First 3 square numbers: " + str(square_numbers[:3]))
+print("Last 3 square numbers: " + str(square_numbers[-3:]))
+
+# iterate through slice
+print("\nHere are the first three square numbers:")
+for square in square_numbers[:3]:
+    print("\t" + str(square))
+
+# copy lists
+new_digits = digits[:]
+print("\nOriginal list: " + str(digits))
+print("Copied list: " + str(new_digits))
+
+# defining tuples
+digits_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
+for digits in digits_tuple:
+    print(str(digits), end = " ")
+
+# You can't change specific values in a tuple but you can change
+# the values of the whole tuple
