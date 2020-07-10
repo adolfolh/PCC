57998c5062c3613ff8e62c17906f568b54e066f2
Adolfo <psyal3@nottingham.ac.uk>
2020-07-10 16:10:18 +0200

    Chapter 3

---
+++ hello_world.py
@@ -0,0 +1,38 @@
+#Python 3.7
+intro_msg = ["hello world", "hi universe", "hey python"]
+
+#sort list alphabetically
+#intro_msg.sort()
+
+#sort list alphabetically (reversed)
+#intro_msg.sort(reverse=True)
+
+#sort a list temporarily
+#print(sorted(intro_msg))
+
+#print list in reverse order
+#intro_msg.reverse()
+
+#appending elements to the end of a list
+#intro_msg.append("mic check")
+
+#inserting elements into a list
+#intro_msg.insert(0, "knock knock")
+
+#deleting an element from a list
+#del intro_msg[0]
+
+#popping an element from a list
+#popped_intro_msg= intro_msg.pop()
+
+#pop from any position in a list
+#second_msg = intro_msg.pop(1)
+
+#removing an item by value
+#intro_msg.remove("hi universe")
+
+#finding length of a list
+#print(len(intro_msg))
+
+print(intro_msg[0].title() + "! I'm new to Python...\n")
+print(intro_msg)
